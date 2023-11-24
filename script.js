61e2d024d40b77a18169ae685d59e3e77d0f9ae9
Katasravani <105917453+Katasravani@users.noreply.github.com>
2023-11-24 15:27:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+function changeColors(squareNumber) {
+    for (let i = 1; i <= 3; i++) {
+        const square = document.getElementById(`square${i}`);
+        if (i !== squareNumber) {
+            square.classList.add('coffee');
+        }
+    }
+}
+
+function resetColors(squareNumber) {
+    for (let i = 1; i <= 3; i++) {
+        const square = document.getElementById(`square${i}`);
+        if (i !== squareNumber) {
+            square.classList.remove('coffee');
+        }
+    }
+}
